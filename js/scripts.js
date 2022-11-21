35d2d71b5dbe74cfb745690bc15ab9027d8d4429
New-Tryer <alexandre.dubrulle.2008@gmail.com>
2022-11-21 19:58:16 +0100

    Ajout progress bar css

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,10 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+$(function() {
+    $('progress').each(function() {
+      var max = $(this).val();
+      $(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
+              });
+  });
